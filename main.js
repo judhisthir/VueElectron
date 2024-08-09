62abeea55b8796c89eb38aa80b3c0bfade952731
judo <judobehera0708@gmail.com>
2024-08-09 16:52:52 +0530

    setup complete

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ const { app, BrowserWindow } = require('electron');
 let mainWindow;
 app.whenReady().then(() => {
   mainWindow = new BrowserWindow({
-    width: 300,
-    height: 600,
+    width: 376,
+    height: 720,
     webPreferences: {
       nodeIntegration: true,
     },
   });
+  // win.loadFile('index.html')
   mainWindow.loadURL('http://localhost:5173');
 });
